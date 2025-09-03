2feec543e8518b2bee28d04b8188ac9bfa06f640
liliia <andycraygo@gmail.com>
2025-09-04 00:02:59 +0200

    feat: register FastEndpoints in DI

--- src/RenuMate/Program.cs
+++ src/RenuMate/Program.cs
@@ -1,8 +1,20 @@
+using FastEndpoints;
+using FastEndpoints.Security;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services
+    .AddAuthenticationJwtBearer(s => 
+        s.SigningKey = builder.Configuration["Jwt:SigningKey"])
+    .AddAuthorization()
+    .AddFastEndpoints();
 
 var app = builder.Build();
+
+app.UseAuthentication()
+    .UseAuthorization()
+    .UseFastEndpoints();
 
 if (app.Environment.IsDevelopment())
 {
