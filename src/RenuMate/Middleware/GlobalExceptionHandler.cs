7c35e6d61e46c5fb8176daaae250c64cc1e9f92d
liliia <andycraygo@gmail.com>
2025-09-06 10:52:35 +0200

    feat: add GlobalExceptionHandler

---
+++ src/RenuMate/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RenuMate.Middleware;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext, 
+        Exception exception, 
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(exception, "Exception occured: {Message}", exception.Message);
+
+        var problemDetails= new ProblemDetails
+        {
+            Title = "Server error",
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = "An internal error occured. Please try later."
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+        
+        return true;
+    }
+}
