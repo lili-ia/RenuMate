aeb03f71ce2ce32a8e676fb52abe14735cd70ee4
liliia <andycraygo@gmail.com>
2025-09-23 17:14:00 +0200

    feat(base): add domain events support

--- src/RenuMate/Entities/BaseEntity.cs
+++ src/RenuMate/Entities/BaseEntity.cs
@@ -1,8 +1,19 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using MediatR;
+
 namespace RenuMate.Entities;
 
 public class BaseEntity
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    private readonly List<INotification> _domainEvents = [];
+    [NotMapped]
+    public IReadOnlyCollection<INotification> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(INotification eventItem) => _domainEvents.Add(eventItem);
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
 }
