cac5cd289027f987c6bc864a7fe14d209bccedb9
liliia <andycraygo@gmail.com>
2026-01-29 22:14:28 +0100

    fix: fix startDate type on Subscription

--- src/renumate-frontend/src/composables/useSubscriptions.ts
+++ src/renumate-frontend/src/composables/useSubscriptions.ts
@@ -201,7 +201,7 @@ export function useSubscriptions() {
       plan: 'Monthly',
       customPeriodInDays: null,
       trialPeriodInDays: null,
-      startDate: '',
+      startDate: new Date(),
       note: '',
       cancelLink: '',
       picLink: '',
